c9395178c446848656d09f4ba0be29606a200cd2
heesangs <heesangs84@gmail.com>
2025-04-30 22:41:20 +0900

    Context API 3 - NoData screen

--- screen/FavoritesScreen.js
+++ screen/FavoritesScreen.js
@@ -2,7 +2,7 @@ import MealList from '../components/MealsList/MealList';
 import { FavoritesContext } from '../store/context/favorites-context';
 import { useContext } from 'react';
 import { MEALS } from '../data/dummy-data';
-import { View,StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 function FavoritesScreen() {
   const favoritesMealsCtx = useContext(FavoritesContext);
@@ -11,11 +11,21 @@ function FavoritesScreen() {
     return favoritesMealsCtx.ids.includes(meal.id);
   });
 
-  return(
-  <View style={styles.container}>
-    <MealList items={favoriteMeal} />
-  </View>
-  )
+  if (favoriteMeal.length === 0) {
+    return (
+      <View style={styles.contentsBox}>
+        <View style={styles.textLayout}>
+          <Text style={styles.text}> you have no favorite meals yet.</Text>
+        </View>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <MealList items={favoriteMeal} />
+    </View>
+  );
 }
 
 export default FavoritesScreen;
@@ -24,5 +34,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#281103',
+  },
+  contentsBox: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#281103',
+  },
+  text: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  textLayout: {
+    marginBottom: 80,
   },
 });
